31735437cbb310c1d720944c1c6f255b6fe17350
Alexey Malyshev <lordniken@mail.ru>
2021-11-13 18:20:18 +0300

    Last sensors data method

--- src/services/Meteo/Meteo.ts
+++ src/services/Meteo/Meteo.ts
@@ -1,16 +1,51 @@
 import { getRepository } from 'typeorm';
 
-import { IMeteoControllerData } from '../MeteoController';
+import { MeteoController, IMeteoControllerData } from '../MeteoController';
 import { Meteo as MeteoEntity } from '../../entities';
+import { Sensors } from '../../constants';
 
 export class Meteo {
+  private static repository() {
+    return getRepository(MeteoEntity);
+  }
+
+  static async getMeteoData() {
+    const data = await MeteoController.request(
+      String(process.env.METEO_CONTROLLER_IP),
+      Number(process.env.METEO_CONTROLLER_PORT),
+    );
+
+    return MeteoController.parse(data);
+  }
+
   static async store(meteoData: IMeteoControllerData[]) {
-    const meteoRepository = await getRepository(MeteoEntity);
-
     meteoData.forEach(async (data) => {
-      const newMeteoData = await meteoRepository.create(data);
+      const newMeteoData = await Meteo.repository().create(data);
 
-      await meteoRepository.save(newMeteoData);
+      await Meteo.repository().save(newMeteoData);
     });
+  }
+
+  static async getLastSensorsData() {
+    const sensors = Object.keys(Sensors)
+      .map(Number)
+      .filter((value) => !isNaN(value));
+
+    const lastSensorsData = await Promise.all(
+      sensors.map(async (sensorId) => {
+        const data = await Meteo.repository().findOne({
+          where: { sensorId },
+          order: { created: 'DESC' },
+        });
+
+        return {
+          temp: data?.temp,
+          humi: data?.humidity,
+          update: new Date(data?.created || ''),
+        };
+      }),
+    );
+
+    return lastSensorsData;
   }
 }
--- src/services/cronJob/cronJob.ts
+++ src/services/cronJob/cronJob.ts
@@ -1,21 +1,11 @@
-import { MeteoController } from '../MeteoController';
 import { Meteo } from '../Meteo';
 
 export class CronJob {
   static readonly schedule = '1 * * * * *';
 
-  private static async getMeteoData() {
-    const data = await MeteoController.request(
-      String(process.env.METEO_CONTROLLER_IP),
-      Number(process.env.METEO_CONTROLLER_PORT),
-    );
-
-    return MeteoController.parse(data);
-  }
-
   static async run() {
     try {
-      const data = await CronJob.getMeteoData();
+      const data = await Meteo.getMeteoData();
 
       Meteo.store(data);
     } catch (error) {
