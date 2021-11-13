7081ef09559531792c4ae10bb294c53988f00654
Alexey Malyshev <lordniken@mail.ru>
2021-11-13 13:03:52 +0300

    Database init

---
+++ ormconfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ormConfig: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  entities: ['src/entities/*.ts'],
+  migrations: ['src/migrations/*.ts'],
+};
+
+export default ormConfig;
