0bb7061a6660c5c9551fa9d30654eeb471524b89
Alexey Malyshev <lordniken@mail.ru>
2021-11-14 14:05:18 +0300

    Added mocked data to status controller for test

--- server/src/controllers/status.ts
+++ server/src/controllers/status.ts
@@ -1,7 +1,17 @@
 import { Meteo } from '../services/Meteo';
 
 export const requestStatus = async () => {
+  const MOCKED = {
+    alarms: {
+      id: '',
+      type: '',
+      time: '',
+    },
+  };
+
   return {
+    ...MOCKED,
+    balance: 0,
     meteoinfo: await Meteo.getLastSensorsData(),
   };
 };
