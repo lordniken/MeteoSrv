238b2e749695c90f3f55b0ead111a48a012a6491
Alexey Malyshev <lordniken@mail.ru>
2021-11-15 00:13:20 +0300

    Added settings controller

--- server/src/controllers/router.ts
+++ server/src/controllers/router.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 
-import { requestStatus } from './status';
+import { Routes } from '../constants';
+
 import { requestChart } from './chart';
+import { requestSettings } from './settings';
+import { requestStatus } from './status';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   // phone app without REST support :-(
   switch (req.query.action) {
-    case 'get_graph_data': {
+    case Routes.chart: {
       const chart = await requestChart(
         String(req.query.date1),
         String(req.query.date2),
@@ -17,6 +20,13 @@ router.get('/', async (req, res) => {
 
       return res.status(200).json(chart);
     }
+
+    case Routes.settings: {
+      const settings = await requestSettings();
+
+      return res.status(200).json(settings);
+    }
+
     default: {
       const status = await requestStatus();
 
---
+++ server/src/controllers/settings.ts
@@ -0,0 +1,13 @@
+export const requestSettings = () => {
+  return {
+    d0min: 0,
+    d0max: 100,
+    d1min: 1,
+    d1max: 101,
+    d2min: 2,
+    d2max: 102,
+    tout: 5,
+    tbtwnalarms: 6,
+    refresh: 5,
+  };
+};
