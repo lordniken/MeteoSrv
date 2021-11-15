2e1e79bfe9a2b69c2b6a0bb74b1c9e99deb195ad
Alexey Malyshev <lordniken@mail.ru>
2021-11-16 00:24:14 +0300

    Timezones

--- server/src/controllers/chart.ts
+++ server/src/controllers/chart.ts
@@ -1,4 +1,4 @@
-import { format } from 'date-fns';
+import { format, zonedTimeToUtc } from 'date-fns-tz';
 
 import { Meteo } from '../services/Meteo';
 
@@ -9,25 +9,21 @@ export const requestChart = async (
 ) => {
   const [startDay, startMonth, startYear] = date1.split('.');
   const [endDay, endMonth, endYear] = date2.split('.');
-  const startDate = new Date(
-    Number(startYear),
-    Number(startMonth) - 1,
-    Number(startDay),
-    0,
-    0,
+  const startDate = zonedTimeToUtc(
+    new Date(Number(startYear), Number(startMonth) - 1, Number(startDay), 0, 0),
+    'Europe/Moscow',
   );
-  const endDate = new Date(
-    Number(endYear),
-    Number(endMonth) - 1,
-    Number(endDay),
-    23,
-    59,
+  const endDate = zonedTimeToUtc(
+    new Date(Number(endYear), Number(endMonth) - 1, Number(endDay), 23, 59),
+    'Europe/Moscow',
   );
   const chartData = await Meteo.getChartData(startDate, endDate, sensorId);
 
   return chartData.map((data) => ({
     temp: data.temp,
     humi: data.humidity || 0, // mobile app crashes if return null :-(
-    time: format(data.created, 'dd.MM.yyyy - HH:mm'),
+    time: format(data.created, 'dd.MM.yyyy - HH:mm', {
+      timeZone: 'Europe/Moscow',
+    }),
   }));
 };
